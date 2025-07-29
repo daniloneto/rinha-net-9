c2e21d9ba4425dddebcc90d31ea5c6618b3ecef3
Danilo Neto <daniloneto@gmail.com>
2025-07-29 14:56:41 -0300

    changes

--- gateway/Repository.cs
+++ gateway/Repository.cs
@@ -25,8 +25,8 @@ public class Repository
             };
             var json = JsonSerializer.Serialize(dbRequest, AppJsonSerializerContext.Default.DatabasePaymentRequest);
             using var content = new StringContent(json, Encoding.UTF8, "application/json");
-            _httpClient.PostAsync("/payments/default", content); // fire-and-forget
-            return await Task.FromResult(true);
+            await _httpClient.PostAsync("/payments/default", content);
+            return true;
         }
         catch
         {           
@@ -45,8 +45,8 @@ public class Repository
             };
             var json = JsonSerializer.Serialize(dbRequest, AppJsonSerializerContext.Default.DatabasePaymentRequest);
             using var content = new StringContent(json, Encoding.UTF8, "application/json");
-            _httpClient.PostAsync("/payments/fallback", content); 
-            return await Task.FromResult(true);
+            await _httpClient.PostAsync("/payments/fallback", content);
+            return true;
         }
         catch
         {           
